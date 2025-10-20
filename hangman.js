a6728c1f382c781d9af6c17df29fd34eecce6286
diannacc <diannacc@nyit.edu>
2025-10-20 10:34:04 -0400

    added guess letter functionality

--- hangman.js
+++ hangman.js
@@ -6,11 +6,36 @@ var word="";
 function newGame(){
     var randomIndex = parse(Math.random() * POSSIBLE_WORDS.length);
     word = POSSIBLE_WORDS[randomIndex];
+    guesses = "";
+    updatePage();
+}
 
+function guessLetter(){
+    var input = document.getElementById("guess");
+    var letter = input.ariaValueMax;
+    guess+=letter;
+    updatePage();
+
+}
+
+
+function updatePage(){
     var clueString = "";
     for(var i =0;i<word.length;i++){
+        var currentLetter = word.charAt(i);
+        if(guesses.indexOf(currentLetter) >= 0){//You Guessed it}
+            clueString += currentLetter + " ";
+
+        }
+        else{
+            clueString += "_ ";
+        }
+
         clueString+= "_ ";
     }    
     var clue = document.getElementById("clue");
     clue.innerHTML = clueString;
+
+    var guessArea = document.getElementById("guesses");
+    guessArea.innerHTML = "Guessed Letters: " + guesses;
 }
