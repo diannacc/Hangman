09336d9dd4610bfb43eeee7c324895a07e8ae5c6
diannacc <diannacc@nyit.edu>
2025-10-22 09:53:44 -0400

    added image update functionality

--- hangman.js
+++ hangman.js
@@ -1,7 +1,9 @@
 var POSSIBLE_WORDS = ["obdurate","verisimilitude","defenestrate","obsequious","dissonant","toady","idempotent"];
 
-
+var MAX_GUESSES = 6;
 var word="";
+var guesses = "";
+var guess_count = MAX_GUESSES;
 
 function newGame(){
     var randomIndex = parse(Math.random() * POSSIBLE_WORDS.length);
@@ -13,6 +15,9 @@ function newGame(){
 function guessLetter(){
     var input = document.getElementById("guess");
     var letter = input.ariaValueMax;
+    if(word.indexOf(letter)<0){
+        guess_count--;
+    }
     guess+=letter;
     updatePage();
 
@@ -32,10 +37,17 @@ function updatePage(){
         }
 
         clueString+= "_ ";
-    }    
+    }
+    //update the clue string    
     var clue = document.getElementById("clue");
     clue.innerHTML = clueString;
 
+    //update the guesses from the user
     var guessArea = document.getElementById("guesses");
     guessArea.innerHTML = "Guessed Letters: " + guesses;
+
+    //update the image
+    var image = document.getElementById("hangmanImage");
+    image.src = "images/hangman" + guess_count + ".gif";
+    
 }
